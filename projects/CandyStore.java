b26808acf6414bd0be54a7a3dedade02b1dbc781
HabShanty <Haborymerstetm@gmail.com>
2024-10-27 20:46:14 -0400

    LeetCode finish

--- projects/CandyStore.java
+++ projects/CandyStore.java
@@ -3,33 +3,36 @@ class CandyStore {
 
         int solution = 0; 
         int corpusLength = ratings.length;
-        int index, prevIndex, nextIndex = 0;
         int[] candies = new int[corpusLength];
         
         //If there is only one child
         if(corpusLength == 1) return 1;
 
-
         //Compare each child to their left neighbor. 
         //If a child’s rating is higher, they get more candies than the left neighbor. 
         //Otherwise, they keep at least one candy.
+        candies[0] = 1; // Give the 0th spot of the array a candy to begin.
         for(int i = 1; i < corpusLength; i++){
-
+            candies[i] = 1;
             
             if( ratings[i] > ratings[i - 1]) {
-                candies[i] += 1;
-                
+                candies[i] = candies[i - 1] + 1;
             }
-            System.out.println(candies[i] + " " + candies[i + 1]);
         }
 
         //Compare each child to their right neighbor. If a child’s rating is higher, 
         //you need to ensure the candy count stays valid by updating it to the max of its current value 
         //and one more than the right neighbor’s candies.
-
-        for(int i = corpusLength - 1; i > -1; i--){
+        for(int i = corpusLength - 2; i > -1; i--){
             
+            if( ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                candies[i] = candies[i + 1] + 1;
+            }
+
+            solution += candies[i];
         }
+        solution += candies[corpusLength - 1];
+
         return solution;
     }
 
@@ -38,5 +41,11 @@ class CandyStore {
 
         int[] children1 = {1, 0, 2};
         System.out.println(solution.candy(children1));  // Output: 5
+
+        int[] children2 = {1, 2, 2};
+        System.out.println(solution.candy(children2));  // Output: 4
+
+        int[] children3 = {1,3,4,5,2};
+        System.out.println(solution.candy(children3));  // Output: 11
     }
 }
